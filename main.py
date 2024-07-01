d8476382191e760e91136ed3db4271ff6f2f71e2
NotREALIFE <notdrakzz@proton.me>
2024-07-01 22:04:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,47 +5,35 @@ import logging
 app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def handle_request():
     target_url = None
-    print("HI1")
     data = request.json
-    print("data:", data)
+    
     if data and 'RedirectingServerTo' in data:
         target_url = data['RedirectingServerTo']
-
-        print("HI2", target_url)
-
         forwarded_data = {k: v for k, v in data.items() if k != 'RedirectingServerTo'}
         
-        # Extract headers and cookies
-        headers = {
-            'User-Agent': request.headers.get('User-Agent', ''),
-            'Accept': request.headers.get('Accept', '')
-        }
+        headers = {key: value for key, value in request.headers.items() if key != 'Host'}
         cookies = request.cookies
         
-        print("OK")
-
         try:
             if request.method == 'POST':
-                print("POST")
                 response = requests.post(target_url, json=forwarded_data, headers=headers, cookies=cookies, timeout=10)
             else:
-                print("ELSE", target_url, forwarded_data, headers, cookies)
-                response = requests.get(str(target_url), params=forwarded_data, headers=headers, cookies=cookies, timeout=10)
+                response = requests.get(target_url, params=forwarded_data, headers=headers, cookies=cookies, timeout=10)
                 
-            # Create a Flask response with the response from the target server
-            flask_response = make_response(response.content, response.status_code)
-            
-            # Copy headers from the target server response
-            for key, value in response.headers.items():
-                flask_response.headers[key] = value
-            
-            return flask_response
+            result = {'Text': response.text}
+            return result, response.status_code
+        
         except requests.exceptions.RequestException as e:
             logging.error(f"Error forwarding request: {e}")
             return 'Error forwarding request', 500
+        
+        except Exception as e:
+            logging.error(f"Unhandled exception: {e}")
+            return 'Internal Server Error', 500
+        
     else:
         return 'No RedirectingServerTo specified', 400
 
